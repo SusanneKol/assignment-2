cfa5565189b2e6420e1b3efddc9ce976a34745fa
Susanne Kolberg <susankol@stud.ntnu.no>
2023-11-05 14:11:15 +0100

    oppdateringer

--- backend/main.tf
+++ backend/main.tf
@@ -9,9 +9,20 @@ terraform {
 
 provider "azurerm" {
   features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
   }
+
 }
 
+
+resource "random_string" "random_string" {
+  length = 10
+  special = false
+  upper = false
+}
 
 resource "azurerm_resource_group" "rg_backend" {
   name     = var.rg_backend_name
@@ -19,7 +30,7 @@ resource "azurerm_resource_group" "rg_backend" {
 }
 
 resource "azurerm_storage_account" "sa_backend" {
-  name                     = var.sa_backend_name
+  name                     = "${lower(var.sa_backend_name)}${random_string.random_string.result}"
   resource_group_name      = azurerm_resource_group.rg_backend.name
   location                 = azurerm_resource_group.rg_backend.location
   account_tier             = "Standard"
@@ -32,4 +43,46 @@ resource "azurerm_storage_container" "sc_backend" {
   name                  = var.sc_backend_name
   storage_account_name  = azurerm_storage_account.sa_backend.name
   container_access_type = "private"
+}
+
+data "azurerm_client_config" "current" {}
+
+
+resource "azurerm_key_vault" "kv_backend" {
+  name                        = "${lower(var.kv_backend_name)}${random_string.random_string.result}"
+  location                    = azurerm_resource_group.rg_backend.location
+  resource_group_name         = azurerm_resource_group.rg_backend.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get", "List","Create",
+    ]
+
+    secret_permissions = [
+      "Set",
+      "Get",
+      "Delete",
+      "Purge",
+      "Recover"
+    ]
+
+    storage_permissions = [
+      "Get", "List","Create",
+    ]
+  }
+}
+
+resource "azurerm_key_vault_secret" "sa_backend_access_key" {
+  name         = var.sa_backend_accesskey_name
+  value        = azurerm_storage_account.sa_backend.primary_access_key
+  key_vault_id = azurerm_key_vault.kv_backend.id
 }
--- backend/variables.tf
+++ backend/variables.tf
@@ -19,3 +19,13 @@ variable "sc_backend_name" {
     type = string
     description = "(optional) describe your variable"
 }
+
+variable "kv_backend_name" {
+    type = string
+    description = "(optional) describe your variable"
+}
+
+variable "sa_backend_accesskey_name" {
+    type = string
+    description = "(optional) describe your variable"
+}
